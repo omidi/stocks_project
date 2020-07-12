5f94d3df85d63d282afef1eb832b10a3020c1480
somidi <saeed.omidi@gmail.com>
2020-07-12 15:27:00 +0200

    testing the alpha vantage package

--- test.py
+++ test.py
@@ -1,1 +1,6 @@
-print("Hello world!")
+from alpha_vantage.timeseries import TimeSeries
+ts = TimeSeries(key='YOUR_API_KEY')
+# Get json object with the intraday data and another with  the call's metadata
+data, meta_data = ts.get_intraday('GOOGL')
+
+print(data)
